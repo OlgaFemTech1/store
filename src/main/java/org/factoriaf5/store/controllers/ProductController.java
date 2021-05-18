810471a5fefa4755c209c662ee478c4870108f31
Olga Caparrós <caparrosolga@gmail.com>
2021-05-18 15:29:53 +0200

    post probando

--- src/main/java/org/factoriaf5/store/controllers/ProductController.java
+++ src/main/java/org/factoriaf5/store/controllers/ProductController.java
@@ -1,16 +1,31 @@
 package org.factoriaf5.store.controllers;
 
+import org.factoriaf5.store.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
-    public class ProductController {
-@GetMapping("/health")
-    public String check() {
-        return "Hello,I'm Olga";
-        /*
-    System.out.println("Saludos"+check());*/
-}
+public class ProductController {
+
+        public List<Product> products = new ArrayList<>();
+
+    @GetMapping("/products")
+    public List<Product> allProducts() {
+        return products;
+    }
+    @PostMapping("/products")
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
 }
 
+
+
+   /* @GetMapping("/health")
+    public String check() {
+        return "Hello,I'm Olga";*/
